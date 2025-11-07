9e988b7814ed85423c75335ab1eb1a7083d45718
Hayzer3 <lucashyzr@gmail.com>
2025-11-07 10:47:01 -0300

    refactor: Secure credentials using .env and update .gitignore

--- src/config/my_keys.py
+++ src/config/my_keys.py
@@ -1,13 +1,20 @@
 import os
+from dotenv import load_dotenv
 
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY', "AIzaSyBvb3WGdJfWX-4vT-whbi1Vh8KuoNgG4eA")
+# Isso é novo!
+# Ele vai procurar um arquivo .env e carregar as chaves de lá
+# para dentro do 'os.getenv'
+load_dotenv() 
+
+
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 ORACLE_CONFIG = {
-    'user': os.getenv('DB_USER', "rm566358"),
-    'password': os.getenv('DB_PASSWORD', "fiap25"), 
-    'host': os.getenv('DB_HOST', "oracle.fiap.com.br"),
-    'port': 1521,
-    'sid': os.getenv('DB_SID', "ORCL")
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'port': 1521, 
+    'sid': os.getenv('DB_SID')
 }
 
 DATABASE_SCHEMA = """
@@ -15,3 +22,8 @@ TB_CAR_PACIENTE: id_paciente, nome_paciente, celular_paciente
 TB_CAR_PROFISSIONAL_SAUDE: id_profissional, nome_profissional, especialidade_profissional  
 TB_CAR_CONSULTA: id_consulta, id_paciente, id_profissional, data_agenda, status_consulta
 """
+
+# verificação para saber se tudo carregou
+if not GEMINI_API_KEY or not ORACLE_CONFIG['user']:
+    print("ERRO CRÍTICO: As variáveis de ambiente não foram carregadas.")
+ 
